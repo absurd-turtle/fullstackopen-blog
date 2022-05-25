29562256a00e584c8426012f9999324ccabc0f55
absurd-turtle <absurd.turtl3@gmail.com>
2022-05-25 14:58:21 +0300

    refactor into modules

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const dotenv = require('dotenv')
-dotenv.config()
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
 
-const Blog = require('./models/blog')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- models/blog.js
+++ models/blog.js
@@ -1,15 +1,4 @@
 const mongoose = require('mongoose')
-const process = require('process')
-const uri = process.env.MONGODB_URI
-
-console.log('connecting to', uri)
-mongoose.connect(uri)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const blogSchema = new mongoose.Schema({
   title: String,

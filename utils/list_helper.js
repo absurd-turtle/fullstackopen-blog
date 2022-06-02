3a0fb8d3735be8c662a75cc5f9497f7d0b45aae4
absurd-turtle <absurd.turtl3@gmail.com>
2022-06-02 15:23:24 +0300

    implemented 4.3-4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (posts) => {
+  return posts.reduce((previous, current) => {
+    return previous + current.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((previous, current) => {
+    return current.likes > previous.likes ? current : previous
+  })
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
